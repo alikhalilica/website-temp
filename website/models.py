9802fbeb53699fc33e1da72ff7dce4a58f56ca5e
alikhalilica <samavat26>
2020-07-04 10:36:17 -0500

    admin

--- website/models.py
+++ website/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class contact(models.Model):
+    message = models.TextField() 
+    name = models.CharField(max_length=100)
+    Email = models.EmailField()
+    subject = models.CharField(max_length=100)
--- website/views.py
+++ website/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from .forms import ContactForm
+
 
 # Create your views here.
 def index (request):
     return render(request,"website/index.html")
 
 def contact (request):
-    return render(request,"website/contact.html")
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path)
+   
+    else:    
+        form=ContactForm()
+        context={"form":form}
+        return render(request,"website/contact.html",context)
+
 
 def about(request):
     return render(request,"website/about.html")
